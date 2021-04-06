ec8b82573ce8ef5e07b942bc10e283625427cec0
fakhruddin07 <fakhruddinnoman95@gmail.com>
2021-04-06 16:17:41 +0600

    edit index file

--- index.js
+++ index.js
@@ -10,10 +10,6 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9cclq.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
